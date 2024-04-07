d4882baec64d47ee3c1750c196b56b60199d9ca2
trishal2154 <148067104+trishal2154@users.noreply.github.com>
2024-04-07 14:59:47 +0530

    Update app.py

--- MFER/app.py
+++ MFER/app.py
@@ -17,11 +17,11 @@ json_file.close()
 classifier = model_from_json(loaded_model_json)
 
 # load weights into new model
-classifier.load_weights("emotion_model1.h5")
+classifier.load_weights("MFER/emotion_model1.h5")
 
 #load face
 try:
-    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    face_cascade = cv2.CascadeClassifier('MFER/haarcascade_frontalface_default.xml')
 except Exception:
     st.write("Error loading cascade classifiers")
 
